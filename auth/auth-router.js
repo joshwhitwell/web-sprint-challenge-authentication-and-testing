e7a14ebe258f3a75ff09e966ba0c291935450005
Joshua Whitwell <josh.whitwell@gmail.com>
2020-11-13 11:47:07 -0500

    adds //auth/register

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,13 +1,20 @@
 const bcrypt = require('bcryptjs')
-
 const jwt = require('jsonwebtoken')
 
-const { jwtSecret } = require('./auth-secrets')
-
 const router = require('express').Router();
+const { jwtSecret } = require('./auth-secrets')
+const Users = require('../users/user-model')
 
-router.post('/register', (req, res) => {
-  // implement registration
+router.post('/register', async (req, res) => {
+  try {
+    const { password } = req.body
+    const hash = bcrypt.hashSync(password, 10)
+    const creds = { ...req.body, password: hash }
+    const user = await Users.add(creds)
+    res.status(201).json(user)
+  } catch (err) {
+    res.status(500).json({ message: err.message, stack: err.stack })
+  }
 });
 
 router.post('/login', (req, res) => {
--- users/user-model.js
+++ users/user-model.js
@@ -4,6 +4,16 @@ function find() {
     return db('users')
 }
 
+async function add(creds) {
+    try {
+        const [id] = await db('users').insert(creds)
+        return find().where({ id }).first()
+    } catch (err) {
+        throw err
+    }
+}
+
 module.exports = {
-    find
+    find,
+    add
 }
