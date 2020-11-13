dbba76741c62bef4b559fb8edcd78636d0537aa8
Joshua Whitwell <josh.whitwell@gmail.com>
2020-11-13 13:46:23 -0500

    adds post /register and post /login tests to auth-router.test.js

--- auth/auth-router.test.js
+++ auth/auth-router.test.js
@@ -13,10 +13,24 @@ describe('auth-router.js module', () => {
         expect(process.env.DB_ENV).not.toBe('production')
     })
 
-    test('[POST] /register works', () => {
+    test('[POST] /register returns new user', () => {
         return request(server).post('/api/auth/register')
             .send({ username: 'shannon', password: 'password' })
             .expect('Content-Type', /json/)
             .expect(201)
+            .then(res => {
+                expect(res.body).toMatchObject({ id: 1, username: 'shannon' })
+            })
+    })
+
+    test('[POST] /login returns token', async () => {
+        await request(server).post('/api/auth/register')
+            .send({ username: 'shannon', password: 'password' })
+        return request(server).post('/api/auth/login')
+            .send({ username: 'shannon', password: 'password' })
+            .expect(200)
+            .then(res => {
+                expect(res.body.token).toBeDefined()
+            })
     })
 })
